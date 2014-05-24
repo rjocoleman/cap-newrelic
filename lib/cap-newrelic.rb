763c9c1ed62a7b164dcb23aa3bb94ac0f51f3716
Robert Coleman <github@robert.net.nz>
2014-05-24 18:49:32 +1200

    fix Rails autoload issue

--- lib/cap-newrelic.rb
+++
@@ -1,4 +0,0 @@
-require 'faraday'
-require 'rexml/document'
-
-load File.expand_path('../cap-newrelic/tasks/cap-newrelic.rake', __FILE__)
---
+++ lib/capistrano/newrelic.rb
@@ -0,0 +1,4 @@
+require 'faraday'
+require 'rexml/document'
+
+load File.expand_path('../tasks/newrelic.rake', __FILE__)
